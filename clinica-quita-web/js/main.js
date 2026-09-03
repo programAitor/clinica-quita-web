35a796e97032acae3b0d62efb38337d64c5f7b71
programAitor <aitorgm.1997@gmail.com>
2026-09-03 19:02:18 +0200

    Delete clinica-quita-web directory

--- clinica-quita-web/js/main.js
+++
@@ -1,4 +0,0 @@
-// Mensaje de consola para comprobar que el script carga correctamente
-console.log("Clínica-Quita cargada correctamente con código puro.");
-
-// Puedes añadir aquí en el futuro la lógica del menú móvil o validaciones del formulario
